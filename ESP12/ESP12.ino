e6e1ffa421a1a5ccb1fa7dacf842d1f6d8e31767
SecretKr <kraiwit.wit@gmail.com>
2024-05-13 23:02:20 +0700

    add esp12 test code

---
+++ ESP12/ESP12.ino
@@ -0,0 +1,55 @@
+// Wifi
+#include <ESP8266WiFi.h>
+#include <WiFiManager.h>
+
+// Firebase
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+#define DATABASE_URL "https://embed-final-project-d2b6c-default-rtdb.asia-southeast1.firebasedatabase.app"
+FirebaseData fbdo;
+FirebaseConfig config;
+FirebaseAuth auth;
+#include "arduino_secrets.h"
+
+// NTP Time
+#include <time.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+WiFiUDP ntpUDP;
+const long  gmtOffset_sec = 25200;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", gmtOffset_sec);
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFiManager wm;
+  bool res;
+  res = wm.autoConnect("ESP12-wifi-setup","password");
+  if(!res) Serial.println("Failed to connect");
+  else Serial.println("Connected");
+  
+  config.api_key = FIREBASE_API;
+  config.database_url = DATABASE_URL;
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("Firebase OK");
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+  config.token_status_callback = tokenStatusCallback;
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+}
+
+int i = 0;
+
+void loop() {
+  timeClient.update();
+  unsigned long epochTime = timeClient.getEpochTime();
+  FirebaseJson json;
+  json.set("x", ++i);
+  Serial.println("send " + String(i));
+  if(!Firebase.RTDB.set(&fbdo, "logs/"+ String(epochTime), &json)) Serial.println("Firebase set log error");
+  delay(5000);
+}
